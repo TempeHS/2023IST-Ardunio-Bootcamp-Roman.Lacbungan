751d87881a83db34d921f0558e40062ad15146c0
RomanLac <roman.lacbungan@education.nsw.gov.au>
2023-08-23 09:38:20 +1000

    activity ten complete

--- 10.servoMotor/10.servoMotor.ino
+++ 10.servoMotor/10.servoMotor.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Roman Lacbungan
   Learning Intention: The students will learn how to connect and control a servo motor.
   Success Criteria:
     1. I understand how to connect the servo motor
@@ -7,8 +7,8 @@
     3. I can map a potentiometer to a servo and control its movement
     4. I understand that a 180deg servo angle of movement is set by a frequency signal sent from the microcontroller
 
-  Student Notes: 
-
+  Student Notes:
+  servo instantly inherits qualities
   Documentation:
     https://www.sparkfun.com/servos
     https://github.com/arduino-libraries/Servo <-- We are still using this library
@@ -18,14 +18,40 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/10.servoMotor/Bootcamp-servoMotor.png
 */
 
-//The Servo-h library is built into Ardunio IDE there is no need to side load it like the Ultrasonic Sensor library
+#include <Servo.h>
+// creates a servo object
+Servo myServo;
 
+// pin server is attached to
+unsigned static int myServoPin = 8;
 
 void setup() {
+// setup and debug serial monitor
+Serial.begin(9600);
+Serial.println("Serial monitor configured to 9600 o/");
+Serial.println("---------------");
 
+// configure myServor to tell pin
+myServo.attach(myServoPin);
 }
 
 // The loop function runs over and over again forever
 void loop() {
+  // return servo to 0 degrees
+  int val = 0;
+  myServo.write(val);
+  delay(250);
 
+  // slowly sweep the servo to 180 degrees
+  for (int i = 0; i <= 180; i++) {
+    myServo.write(i);
+    delay(30);
+
+  // slowly sweep the servo to 180 degrees
+  for (int y = 180; y <= 0; y++) {
+    myServo.write(y);
+    delay(30);
+
+    }
+  }
 }
